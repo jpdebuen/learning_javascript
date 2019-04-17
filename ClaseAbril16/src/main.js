14499c896ff7c30c7ada19e872a657a3d76f65bd
jpdebuen <jorge@bluepixel.mx>
2019-04-16 19:59:31 -0500

    Ejercicio de funciones que cambian dólares a pesos y luego pesos a litros de gasolina.

--- ClaseAbril16/src/main.js
+++ ClaseAbril16/src/main.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+
+
+
+//Función que transforma dólares a pesos
+function dolaresPesos(dolares){
+    return (18.87*dolares);
+};
+
+
+//Función que transforma pesos mexicanos a litros de gasolina
+function compraGasolina(dinero){
+    return dinero/19.37;
+};
+
+console.log("Te alcanza para ", compraGasolina(dolaresPesos(100)), "litros de la verde.");
