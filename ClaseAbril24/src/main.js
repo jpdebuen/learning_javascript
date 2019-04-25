fb91917328d260a1c4b562986bbf7c9c42fc90c6
jpdebuen <jp.debuen@gmail.com>
2019-04-24 22:03:47 -0500

    ejercicios de DOM y ejercicio no resuelto de constructor de div con contenido

--- ClaseAbril24/src/main.js
+++ ClaseAbril24/src/main.js
@@ -1,5 +1,42 @@
+
+/*
 console.log(document.getElementsByTagName("div"))
 console.log(document.getElementsByTagName("ul"))
 console.log(document.getElementsByTagName("li"))
 console.log(document.getElementsByClassName("even"))
 console.log(document.getElementsByTagName("li")[3])
+*/
+
+
+/*
+var cont_divs = document.getElementsByClassName("cont");
+for (var i = 0; i <= cont_divs.length - 1; ++i){
+ cont_divs[i].style.margin = '100px';
+}
+*/
+
+/*
+var li_collection = document.getElementsByTagName("li");
+
+for (var i = 0; i <= li_collection.length - 1; ++i){
+  li_collection[i].style.color = 'red';
+}
+*/
+
+
+
+var node = document.createElement("h1");
+var txt = document.createTextNode("Create World");
+
+node.appendChild(txt);
+
+
+var nodeObj = {
+    "tag": "div",
+    "children": [
+        {
+            "tag": "h2",
+            "text": "Hello"
+        }
+    ]
+}
