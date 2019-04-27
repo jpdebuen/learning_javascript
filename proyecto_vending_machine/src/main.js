7192874395b7ed5ddcc98792fec82189919ab076
jpdebuen <jorge@bluepixel.mx>
2019-04-27 13:28:31 -0500

    grid con tarjetas de productos

---
+++ proyecto_vending_machine/src/main.js
@@ -0,0 +1,25 @@
+var vending_machine ={
+    "columns": 4,
+    "rows": 6,
+    "slotCapacity": 10,
+    "stock": [
+        {
+            "id": "11",
+            "price": 20,
+            "counter": 5,
+            "nombre": "Producto",
+            "image": "./img/11",
+            "column": 1,
+            "row": 1,
+        },
+        {
+            "id": "12",
+            "price": 20,
+            "counter": 5,
+            "nombre": "Producto",
+            "image": "./img/12",
+            "column": 2,
+            "row": 1,
+        },
+    ]
+}
