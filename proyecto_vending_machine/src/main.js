7e757d1aaab88a1bf01a4103286897026b76fe1d
jpdebuen <jorge@bluepixel.mx>
2019-04-27 17:10:18 -0500

    función de ompra completada

--- proyecto_vending_machine/src/main.js
+++ proyecto_vending_machine/src/main.js
@@ -7,7 +7,7 @@ var vending_machine ={
             "id": "11",
             "price": 20,
             "counter": 5,
-            "nombre": "Producto",
+            "name": "Sabritas Originales",
             "image": "./img/11",
             "column": 1,
             "row": 1,
@@ -16,10 +16,56 @@ var vending_machine ={
             "id": "12",
             "price": 20,
             "counter": 5,
-            "nombre": "Producto",
+            "name": "Doritos Nacho",
             "image": "./img/12",
             "column": 2,
             "row": 1,
         },
     ]
 }
+
+
+/* FUNCIONES */
+
+function getProduct(id){
+    return vending_machine.stock.find(function(element){
+        return element.id === id;
+    });
+}
+
+function checkChange(id, money){
+    if (getProduct(id).price < money){
+        return money - getProduct(id).price;
+    }
+}
+function checkCounter(id){
+    return getProduct(id).counter != 0;
+}
+
+function buy(id, money){
+    var product = getProduct(id);
+    var isInStock = checkCounter(id)
+    var isEnoughMoney = product && (money >= product.price)
+
+    if (!product) {
+        console.log('No existe el producto')
+        return 
+    }
+
+    if (!isInStock) {
+        console.log('El producto esta agotado')
+        return 
+    }
+
+    if (!isEnoughMoney) {
+        console.log('No es suficiente dinero')
+        return
+    }
+
+    product.counter --;
+    console.log("Disfruta tu(s) " + product.name + "!");
+    console.log("Tu cambio es: $" + checkChange(id, money));
+
+}
+
+
