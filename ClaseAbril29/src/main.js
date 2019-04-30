0bc64b7b2eab5795de24a9828b271db0c24cb9ba
jpdebuen <jorge@bluepixel.mx>
2019-04-29 20:02:54 -0500

    js and bootstrap files

---
+++ ClaseAbril29/src/main.js
@@ -0,0 +1,15 @@
+var car = {
+    "brand": "Honda",
+    "model": {
+        "line": "Fit",
+        "submodel": "Fun",
+        "year": 2018
+    },
+    "color": {
+        "name": "white",
+        "hex": "FFFFFF",
+        "finish": "pearlescent"
+    },
+    "doors": 5,
+    "price": 280000,
+}
