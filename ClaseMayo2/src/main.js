0b60b49b6cb51fd91853addedaba33292f89ff92
jpdebuen <jorge@bluepixel.mx>
2019-05-02 21:31:29 -0500

    objeto de mascota completo. No se imprime en el DOM.

--- ClaseMayo2/src/main.js
+++ ClaseMayo2/src/main.js
@@ -1,85 +1,79 @@
-/*
-setTimeout(function(){
-    console.log("Hello");
-}, 3000)
 
-var interval = setInterval(function(){
-    console.log("Hellooou");
-}, 3000)
-*/
-
-
-        //document.querySelectorAll("#time").innerHTML = time;
-
-
-
-function startTimer(seconds, minutes){
-    var timer = (setInterval(function(){
-        if(seconds === 0){
-            clearInterval(timer);
-        };
-        console.log(seconds);
-        seconds--;
-    }, 1000));
+var petObj = {
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/c/cc/ACD-blue-spud.jpg",
+    "name": "Shanti",
+    "sex": "Female",
+    "breed":{
+        "breedName": "Blue Heeler",
+        "pedigree": true
+    },
+    "birthDate": "09/09/2005",
+    "color": "brown",
+    "owners":[
+        {
+        "name": "Jorge",
+        "address": "Paseo de los Tamarindos 218",
+        "phone": "4422490259",
+        "email": "jp.debuen@gmail.com"
+        },
+    ],
+    "appointment":{
+        "date": "05/15/2019",
+        "time": "16:00",
+        "recurrent": "true"
+    },
+    "vaccines":[
+        {
+            "name": "Canine Influenza Virus-H3N2",
+            "applied": true
+        },
+        {
+            "name": "Bordatella",
+            "applied": true
+        },
+        {
+            "name": "Canine Influenza Virus-H3N8",
+            "applied": true
+        },
+        {
+            "name": "Rabies",
+            "applied": true
+        },
+    ],
+    "sterilized": true
 }
 
-/*
-function startTimer(seconds, minutes){
-    var timer = (setInterval(function(){
-        if(seconds === 0){
-            minutes--
-            //clearInterval(timer);
-        };
-        console.log("0" + minutes + ":" + seconds);
-        seconds--;
-    }, 1000));
-}
-*/
-
-
-var xhttp = new XMLHttpRequest()
-var p_element = document.createElement("p");
-var new_p = document.createElement("p");
-var div_element = document.createElement("div");
-
-xhttp.onreadystatechange = function() {
-    console.log(this.readyState, this.status)
-    if( this.readyState === 4 && this.status === 200) {
-        var objeto = (JSON.parse(this.responseText))
-        console.log(objeto);
-        //función 
-        function insertText(key, id){
-            var element = document.createElement(key);
-            element.innerHTML = objeto.location;
-            document.getElementById(id).appendChild(element)
-        }
-        //vet_name
-        insertText("p", "vet-name");
-        //location
-        insertText("p", "location");
-
-
-    } else if( this.status === 404 ) {
-        console.log('NOPE')
+function request() {
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+      if(this.readyState === 4 && this.status === 200) {
+          console.log(this.response)
+        buildStructure(this.response["-LdvRidGHCWTOddNt9W3"]);
+      }
     }
+    xhttp.open('GET', 'https://ajaxkode.firebaseio.com/jorge.json', true)
+    xhttp.send(/* JSON.stringify(petObj) */)
+  }
+  
+  
+function buildPets(pet) {
+    var allPets = ''
+        var buildPet = ''
+        buildPet +='<div class="card" style="width: 18rem;">'
+        buildPet +='    <div class="card-body">'
+        buildPet +='      <p class="card-text">'+ pet.name +'</p>'
+        buildPet +='      <p class="card-text">'+ pet.sex +'</p>'
+        buildPet +='      <p class="card-text">'+ pet.breed.name +'</p>'
+        buildPet +='    </div>'
+        buildPet +='  </div>'
+        
+        allPets += buildPet
+    document.getElementById('pets-wrapper').innerHTML = allPets;
 }
 
-xhttp.open('GET', 'https://ajaxkode.firebaseio.com/.json', true)
-xhttp.send()
-
-function insertText(key, id){
-    var element = document.createElement(key);
-    element.innerHTML = objeto.location;
-    document.getElementById(id).appendChild(element)
+  
+function buildStructure(result) {
+    var resObj = JSON.parse(result);
+    console.table(resObj["-LdvRidGHCWTOddNt9W3"]);
+    buildPets(resObj["-LdvRidGHCWTOddNt9W3"]);
 }
-
-
-
-/* 
-//vet name 
-p_element.innerHTML = objeto.name;
-document.getElementById("vet-name").appendChild(p_element);
-//location
-new_p.innerHTML = objeto.location;
-document.getElementById("location").appendChild(new_p);
-*/
