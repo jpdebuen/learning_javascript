89140e74d4badfb77f03a7977e373a8a98e4df99
jpdebuen <jorge@bluepixel.mx>
2019-05-06 21:42:03 -0500

    ejercicios de métodos y sintáxis de jquery para hacer el dropdown de una lista

---
+++ ClaseMayo6/src/main.js
@@ -0,0 +1,18 @@
+/*
+$(".dd-button").on("click", ()=>{
+  $("#dropdownOne").slideToggle("400");
+});
+*/
+
+/*
+$(".dd-button").on("click", ()=>{
+  if ($("ul").hasClass("hide")){
+    $("ul").removeClass("hide");
+    $("ul").addClass("show");
+  } else if ($("ul").hasClass("show")){
+    $("ul").removeClass("show");
+    $("ul").addClass("hide");
+  }
+});
+*/
+
